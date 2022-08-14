9044b01174f040a864bbe505924149a62136c19e
flexxor <mail@fbeier.de>
2022-08-14 11:21:41 +0200

    cleanup

--- normalize_test.py
+++ normalize_test.py
@@ -1,15 +1,13 @@
 input = 20
 
 def normalize(value_to_normalize, max_value = 40, min_value = 0, target_max = 255, target_min = 0):
-    if value_to_normalize > max_value:
-        value_to_normalize = max_value
-    if value_to_normalize < min_value:
-        value_to_normalize = 0
+    value_to_normalize = min(value_to_normalize, max_value)
+    value_to_normalize = max(value_to_normalize, min_value)
 
     quotient = value_to_normalize / max_value
 
     target_value = (int) (target_max * quotient)
+    target_value = max(target_value, target_min)
     print(f"Result: {target_value}")
-
 
 normalize(20)
